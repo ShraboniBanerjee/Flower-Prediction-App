eaf26b56a4f24764ef53c655833cb0d669cae70e
ShraboniBanerjee <shilpabanerjee50@gmail.com>
2022-08-01 23:13:08 +0530

    new

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+
+#Simple Streamlit web app for iris flower prediction by sepal & petal length and width
+
+#If u dont hv the following libraries please install it by type the command
+# Pip Install Streamlit,pandas sklearn 
+
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
+st.write("""# Simple Iris Flower Prediction App, 
+This app predicts Iris flower types""")
 
 st.sidebar.header('User Input Parameters')
 
